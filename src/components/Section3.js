6c9c612a7e4fedec0d4300f8ff51e033442edd80
Anton <antonreva846@gmail.com>
2022-09-12 17:09:40 +0300

    section 3 -done

---
+++ src/components/Section3.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Section3 = () => {
+    return (
+        <section className='pray-section'>
+            <div className='container'>
+                <div className="pray-section__body">
+                    <span className='pray-section__subTitle'>SUB-HEADLINE</span>
+                    <h1 className='pray-section__title'>love and compassion</h1>
+                    <p className="pray-section__text">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                    <div className='pray-section__photoalbum-block'>
+                        <button className="pray-section__btn">read more</button>
+                        <div className='pray-section__photoalbum-list'>
+                            <img src="./assets/images/pray-alb1.png" alt="" data-height/>
+                            <img src="./assets/images/pray-alb2.png" alt=""/>
+                            <img src="./assets/images/pray-alb3.png" alt="" data-height/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section3;
