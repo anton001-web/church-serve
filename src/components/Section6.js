b8c08e29ae0b8d9095d1d8279ee7f527fb4c4a37
Anton <antonreva846@gmail.com>
2022-09-13 10:14:18 +0300

    section6 - done

---
+++ src/components/Section6.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Section6 = () => {
+    return (
+        <section className='serve-section'>
+            <div className="serve-section__body">
+                <div className='serve-section__info-block'>
+                    <h1 className='serve-section__title'>We want to serve the world around us</h1>
+                    <p className="serve-section__text">Lorem ipsum dolor sit amet, consectetur <br/> adipiscing elit, sed do.</p>
+                    <button className="serve-section__btn">visit</button>
+                </div>
+                <div className='serve-sec__img-block'>
+                    <img src="./assets/images/quoteIcon.png" alt=""/>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Section6
